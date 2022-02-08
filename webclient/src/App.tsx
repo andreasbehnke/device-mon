98baf784fe5b6802bb774655acfea51ed373c8f6
andreasbehnke <ab@andreasbehnke.com>
2022-02-08 19:37:46 +0100

    Installed material UI and cleaned up app

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Typography} from "@mui/material";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <>
-    </>
+      <Typography variant={"h5"}>Network Device Monitor</Typography>
   );
 }
 
