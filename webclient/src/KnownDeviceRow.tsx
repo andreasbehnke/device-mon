f7e10d95b03fcf60a7322bc434d60049034bdaa2
andreasbehnke <ab@andreasbehnke.com>
2022-02-09 12:28:56 +0100

    Use moment for displaying time values

--- webclient/src/KnownDeviceRow.tsx
+++ webclient/src/KnownDeviceRow.tsx
@@ -1,13 +1,14 @@
 import {IconButton, TableCell, TableRow} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
+import moment from "moment";
 
 export function KnownDeviceRow() {
     return <TableRow>
         <TableCell>64:A2:F9:74:62:BF</TableCell>
         <TableCell>192.168.50.29</TableCell>
         <TableCell>dhcp-untrusted</TableCell>
-        <TableCell>2022-02-07 20:20:29.018+00</TableCell>
+        <TableCell>{moment('2022-02-03 20:20:29.018+00').fromNow()}</TableCell>
         <TableCell>Galaxy-Tab-S5e</TableCell>
         <TableCell>
             <IconButton aria-label="Forget device" title={"Forget device"}>
--- webclient/src/NewDeviceRow.tsx
+++ webclient/src/NewDeviceRow.tsx
@@ -1,14 +1,15 @@
 import {IconButton, TableCell, TableRow, TextField} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import React from "react";
+import moment from "moment";
 
 export function NewDeviceRow() {
     return <TableRow>
         <TableCell>64:A2:F9:74:62:BF</TableCell>
         <TableCell>192.168.50.29</TableCell>
         <TableCell>dhcp-untrusted</TableCell>
-        <TableCell>2022-02-07 20:20:29.018+00</TableCell>
-        <TableCell><TextField variant={"standard"} value={"Galaxy-Tab-S5e"}></TextField></TableCell>
+        <TableCell>{moment('2022-02-07 20:20:29.018+00').fromNow()}</TableCell>
+        <TableCell><TextField variant={"standard"} value={"Galaxy-Tab-S5e"} /></TableCell>
         <TableCell>
             <IconButton aria-label="Approve device" title={"Approve device"}>
                 <AddIcon color={"primary"}/>
