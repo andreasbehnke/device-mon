7332c75e907c55cfe649136bb771eadb1cc0d726
andreasbehnke <ab@andreasbehnke.com>
2022-02-12 15:44:41 +0100

    Use dark theme

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
+import {Box, createTheme, CssBaseline, ThemeProvider, Typography} from "@mui/material";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -7,12 +7,16 @@ import '@fontsource/roboto/700.css';
 import {DeviceListLoader} from "./DeviceListLoader";
 
 class App extends React.Component {
+    theme = createTheme({palette: {mode: "dark"}});
     render() {
         return (
-            <Box sx={{m: 3}}>
-                <Typography variant={"h4"} gutterBottom>Network Device Monitor</Typography>
-                <DeviceListLoader/>
-            </Box>
+            <ThemeProvider theme={this.theme}>
+                <CssBaseline />
+                <Box sx={{m: 3}}>
+                    <Typography variant={"h4"} gutterBottom>Network Device Monitor</Typography>
+                    <DeviceListLoader/>
+                </Box>
+            </ThemeProvider>
         );
     }
 }
