cb4adf323aa2c66bd3ce2c78909c1a416bb9503a
andreasbehnke <ab@andreasbehnke.com>
2022-02-27 19:30:05 +0100

    create docker build for spring boot backend using cached maven repository

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker run -v "$PWD/webclient":/usr/src/app -w /usr/src/app node:17 /bin/bash -c "npm install && npm run build"
+(
+  cd backend
+  DOCKER_BUILDKIT=1 docker build -t devicemon_backend .
+)
+# docker run -v "$PWD/webclient":/usr/src/app -w /usr/src/app node:17 /bin/bash -c "npm install && npm run build"
 
