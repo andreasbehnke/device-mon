7750ecad5c805dcd02beab46639e3cfb8ca768ec
andreasbehnke <ab@andreasbehnke.com>
2022-03-04 21:21:57 +0100

    publish images to github repository

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 (
   cd backend
-  DOCKER_BUILDKIT=1 docker build -t devicemon_backend .
+  export DOCKER_BUILDKIT=1
+  docker build -t devicemon_backend .
+  docker tag devicemon_backend:latest ghcr.io/andreasbehnke/devicemon_backend:latest
+  docker push ghcr.io/andreasbehnke/devicemon_backend:latest
 ) &
 (
   cd webclient
-  DOCKER_BUILDKIT=1 docker build -t devicemon_webclient .
+  export DOCKER_BUILDKIT=1
+  docker build -t devicemon_webclient .
+  docker tag devicemon_webclient:latest ghcr.io/andreasbehnke/devicemon_webclient:latest
+  docker push ghcr.io/andreasbehnke/devicemon_webclient:latest
 ) &
 wait
